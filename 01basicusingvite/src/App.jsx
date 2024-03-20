350e35c954ca591aad14df72231a8327962304e4
GitMukulTyagi <mukultyagidocx@gmail.com>
2024-03-20 11:49:49 +0530

    Adding version 01

---
+++ 01basicusingvite/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello This is Basic in React Learning using vite</h1> 
+      
+  )
+}
+
+export default App
