c666762f55e28f8a952f46d5d15c72672cead78c
GitMukulTyagi <mukultyagidocx@gmail.com>
2024-03-20 11:51:52 +0530

    Adding version 01

---
+++ 03variableinfunction/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello This is chapter 03 in Leaning ReactJS</h1>
+  );
+}
+
+export default App;
---
+++ 03variableinfunction/src/Declarevaribale.js
@@ -0,0 +1,11 @@
+
+function Declarevariable() {
+
+    const user = "This is user name from varibale"
+    return (
+      <h3>Hello! Good Morning "{user}"</h3>
+    );
+  }
+  
+  export default Declarevariable;
+  
